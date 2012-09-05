aef4f44976c3c4c1d22c41c071ca2677b1751932
Alexander Manzyuk <admsev@gmail.com>
2012-09-05 20:06:25 +0300

    fixes

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,9 +1,14 @@
 class Banner < ActiveRecord::Base
   belongs_to :site
   attr_accessible :image, :url, :name, :show_stock, :click_stock
+
+  validates :url, :format => URI::regexp(%w(http https))
   
   validates :site_id, :presence => true
   validates :image, :presence => true
+
+  validates :show_stock, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
+  validates :click_stock, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
   
   mount_uploader :image, ImageUploader
   
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,8 @@
 class Site < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name, :url
+
+  validates :url, :format => URI::regexp(%w(http https))
   
   has_many :banners
   
